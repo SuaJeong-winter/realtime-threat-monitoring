639e86e75e44f6f27448dd94ec1436a5933ddd21
SuaJeong-winter <sua.jeong.developer@gmail.com>
2026-04-29 23:36:17 +0900

    test: security_logs 연동 검증을 위한 테스트 데이터 렌더링 및 화면 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-export default function Home() {
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {data:logs,error} = await supabase.from('security_logs').select('*')
+
+  if(error){
+    console.error("Error fetching logs:", error.message);
+  }else{
+    console.log("호출된 데이터 건수:", logs?.length);
+    console.log("첫 번째 데이터 샘플: ",logs?.[0]);
+  }
+
   return (
-    <div>
+    <main style={{padding:'2rem'}}>
       <h1>보안 관제 대시보드 테스트</h1>
-    </div>
+      {error && <p style={{color:'red'}}>에러 발생:{error.message}</p>}
+
+      <h3>데이터 원본 확인(JSON)</h3>
+      <pre style={{backgroundColor:'#f4f4f4',padding:'15px'}}>
+        {logs? JSON.stringify(logs, null, 2) : "데이터가 없습니다."}
+      </pre>
+    </main>
   );
 }
